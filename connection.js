8aa05da222abb126080cede963c8812c011b84d1
Harrison <mvharrison@labs.neit.edu>
2018-01-18 12:52:45 -0500

    Updated connection.js

--- connection.js
+++ connection.js
@@ -3,7 +3,7 @@ var url = "mongodb://localhost:27017/"
 
 MongoClient.connect(url, function(err, db){
 	if(err) throw err;
-	console.log("Connected to MongoDB");
+	console.log("Connected to MongoDB Hello World");
 db.close();
 
 });
